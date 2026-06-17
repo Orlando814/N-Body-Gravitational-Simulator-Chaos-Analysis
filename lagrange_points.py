6d8dff831167ff210b44690837175f9c96c73fa3
Orlando <lupone.o@northeastern.edu>
2026-06-17 02:10:47 -0400

    Created label func and fixed numpy typing error for newer versions

--- lagrange_points.py
+++ lagrange_points.py
@@ -13,7 +13,7 @@ def two_body_potential_gradient_rot(r: np.ndarray, pos: np.ndarray, mass: np.nda
     du_dz = np.sum(-deriv * 2 * (r[2] - pos[:, 2, np.newaxis]), axis = 0)
     return np.array([du_dx, du_dy, du_dz])
 
-def two_body_potential_rot(r: np.ndarray, pos: np.ndarray, mass: np.ndarray, omega: float, g: float) -> float:
+def two_body_potential_rot(r: np.ndarray, pos: np.ndarray, mass: np.ndarray, omega: float, g: float) -> np.ndarray:
 
     coef = g * mass
 
@@ -46,13 +46,13 @@ def find_lagrange_123(gradient_func, b1_pos: np.ndarray, b2_pos: np.ndarray, m1:
     # Will find indices for lagrange points, find there positions, then create the bracket to solve for 0
     lagrange_points_inx = correct_lagrange_inx(grid, grad_x, pos, mass_ratio, dist_bet_bodies)
     bracket = np.array([grid[lagrange_points_inx], grid[lagrange_points_inx + 1]]).T
-
     # dummy function to use in the brentq since brentq can only pass a single value. We also return only the x value of
     # the gradient becuase we already know at a lagrange point y, z = 0 so we just have to solve where x = 0
     def f(axis):
-            return gradient_func(np.array([axis, 0, 0]), pos, mass, omega, g)[0]
+        out = gradient_func(np.array([float(axis), 0.0, 0.0]), pos, mass, omega, g)
+        return float(out[0].item())
 
-    # We're solving for three points so three iterations. The brentq basically takes a function and a left / right bound
+        # We're solving for three points so three iterations. The brentq basically takes a function and a left / right bound
     # These bounds need to be of the opposite sign as that means there is a 0 somewhere in between them. Then the brentq
     # will solve for a x value which results in a 0 for the provided function based on the two bounds
     for point in range(np.size(bracket, axis = 0)):
@@ -60,7 +60,7 @@ def find_lagrange_123(gradient_func, b1_pos: np.ndarray, b2_pos: np.ndarray, m1:
     return lagrange_points123
 
 # L4-5 form equidist triangle between the two masses present. Pretty cool
-def find_lagrange_45(b1_pos: np.ndarray, b2_pos: np.ndarray, m1: float, m2: float) -> np.ndarray:
+def find_lagrange_45(b1_pos: np.ndarray, b2_pos: np.ndarray) -> np.ndarray:
     avg_dist = (b1_pos[0] + b2_pos[0]) / 2
     actual_dist = abs(b1_pos[0] - b2_pos[0])
     l4 = np.array([avg_dist, np.sqrt(3) / 2 * actual_dist])
@@ -91,10 +91,44 @@ def correct_lagrange_inx(test_pos: np.ndarray, grad_axis: np.ndarray, pos_body: 
     # Filter ou any incorrect points
     return sign_flip_inx[overall_check]
 
-m1, m2, g = 1, 5, 1
+def create_lagrange_labels(lagrange_points: np.ndarray, r1: np.ndarray,  r2: np.ndarray,
+                           m1: float, m2: float) -> list[str]:
+    labels = []
+    if (m1 > m2):
+        heavy_body = r1
+        light_body = r2
+    else:
+        heavy_body = r2
+        light_body = r1
+    for point in lagrange_points:
+
+        # L5 point has negative y value
+        if point[1] < 0:
+            labels.append("L5")
+
+        # L4 point has positive y value
+        elif point[1] > 0:
+            labels.append("L4")
+
+        # L1 is inbetween the two bodies
+        elif ((heavy_body[0] < point[0]) and (point[0] < light_body[0])) or ((heavy_body[0] > point[0]) and (point[0] > light_body[0])):
+            labels.append("L1")
+
+        # L3 is the farthest away from the light body
+        elif np.linalg.norm(light_body[:-1] - point) == max(np.linalg.norm(light_body[:-1] - lagrange_points,
+                                                            axis = 1)):
+            labels.append("L3")
+
+        # L2 is the farthest away from the heavy body
+        elif np.linalg.norm(heavy_body[:-1] - point) == max(np.linalg.norm(heavy_body[:-1] - lagrange_points,
+                                                                           axis = 1)):
+            labels.append("L2")
+    return labels
+
+m1, m2, g = 1, 10, 1
 dist_between_bodies = 10
-r1 = np.array([m2 / (m1 + m2) * dist_between_bodies, 0.0, 0.0])
-r2 = np.array([-m1 / (m1 + m2) * dist_between_bodies, 0.0, 0.0])
+r1 = np.array([-m2 / (m1 + m2) * dist_between_bodies, 0.0, 0.0])
+r2 = np.array([m1 / (m1 + m2) * dist_between_bodies, 0.0, 0.0])
 omega = angular_velocity(dist_between_bodies, m1, m2, g)
 x = np.linspace(-15, 15, 1000)
 y = np.linspace(-15, 15, 1000)
@@ -112,13 +146,12 @@ surf = ax_3d.plot_surface(x_mesh, y_mesh, z_pot, cmap = "viridis")
 contour_lines = ax_con.contour(x_mesh, y_mesh, z_pot, levels = 30, colors = "white", linewidths = 0.7)
 contour = ax_con.contourf(x_mesh, y_mesh, z_pot, levels = 20, cmap = "viridis")
 lagrange_123 = find_lagrange_123(two_body_potential_gradient_rot, r1, r2, m1, m2, omega, g, dist_between_bodies)
-lagrange_45 = find_lagrange_45(r1, r2, m1, m2)
+lagrange_45 = find_lagrange_45(r1, r2)
 lagrange_points = np.concatenate([lagrange_123, lagrange_45], axis = 0)
 lagrange_points_scatter = ax_con.scatter(lagrange_points[:, 0], lagrange_points[:, 1],
                                          color = "red", s = 100, marker = "*")
-lagrange_labels = ["L3", "L1", "L2", "L4", "L5"]
+lagrange_labels = create_lagrange_labels(lagrange_points, r1, r2, m1, m2)
 for point in range(np.size(lagrange_points, axis = 0)):
-
     ax_con.annotate(lagrange_labels[point],
                     (lagrange_points[point][0], lagrange_points[point][1]),
                     xytext = (5, 5),
